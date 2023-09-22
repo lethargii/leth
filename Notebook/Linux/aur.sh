e2fe2e1405c7293db329524c368af288ba19a462
lethargii <l.sevault@hotmail.com>
2023-09-22 14:42:04 +0200

    packages upgrade

--- Notebook/Linux/aur.sh
+++ Notebook/Linux/aur.sh
@@ -1,1 +1,9 @@
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+yay -Y --gendb
+yay -Syu --devel
+yay -Y --devel --save
+yay -S flatpak
+yay -S pamac-all
 yay -S godot-mono-bin labwc logmein-hamachi pamac-all rofi-lbonn-wayland sacad sfwbar adwaita-qt4 adwaita-qt5-git adwaita-qt6-git
