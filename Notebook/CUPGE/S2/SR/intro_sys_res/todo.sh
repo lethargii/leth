228fb19913ced8a96bce76b2beb992666f0f9d3c
lethargii <l.sevault@hotmail.com>
2024-01-21 18:43:54 +0100

    vault backup: 2024-01-21 18:43:54

--- Notebook/CUPGE/S2/SR/intro_sys_res/todo.sh
+++ Notebook/CUPGE/S2/SR/intro_sys_res/todo.sh
@@ -24,36 +24,37 @@ function todo()
 		fi
 		# Ajouter une tâche
 	elif [[ $1 = add ]] ; then
-		if [[ $2 = +([0-9]) ]] ; then
-			if [[ ! $2 -gt $[$(wc -l < $TACHES) + 1] ]] ; then
-				echo La tâche \"${@:3}\" a été ajoutée en position $2.
-				head -n $[$2-1] $TACHES > TEMP
-				echo ${@:3} >> TEMP
-				tail $TACHES -n $[$(wc -l < $TACHES)-$2+1] >> TEMP
-				cat TEMP > $TACHES
-				rm TEMP
-			else
-				echo Nombre trop grand.
-			fi
-		else
+		if [[ ! $2 = +([0-9]) ]] ; then
 			echo Un nombre est attendu.
+			exit 1
 		fi
+		if [[ $2 -gt $[$(wc -l < $TACHES) + 1] ]] ; then
+			echo Nombre trop grand.
+			exit 1
+		fi
+		echo La tâche \"${@:3}\" a été ajoutée en position $2.
+		head -n $[$2-1] $TACHES > TEMP
+		echo ${@:3} >> TEMP
+		tail $TACHES -n $[$(wc -l < $TACHES)-$2+1] >> TEMP
+		cat TEMP > $TACHES
+		rm TEMP
 		# Retirer une tâche
 	elif [[ $1 = done ]] ; then
-		if [[ $2 = +([0-9]) ]] ; then
-			if [[ ! $2 -gt $(wc -l < $TACHES) ]] ; then
-				echo La tâche $2 \($(head -n $2 $TACHES | tail -n 1)\) est faite !
-				head -n $[$2-1] $TACHES > TEMP
-				tail $TACHES -n $[$(wc -l < $TACHES)-$2] >> TEMP
-				cat TEMP > $TACHES
-				rm TEMP
-			else 
-				echo Nombre trop grand.
-			fi
-		else
+		if [[ ! $2 = +([0-9]) ]] ; then
 			echo Un nombre est attendu.
-		fi	
+			exit 1
+		fi
+		if [[ $2 -gt $(wc -l < $TACHES) ]] ; then
+			echo Nombre trop grand.
+			exit 1
+		fi
+		echo La tâche $2 \($(head -n $2 $TACHES | tail -n 1)\) est faite !
+		head -n $[$2-1] $TACHES > TEMP
+		tail $TACHES -n $[$(wc -l < $TACHES)-$2] >> TEMP
+		cat TEMP > $TACHES
+		rm TEMP
 	else
 		echo $1 n\'est pas une commande TODO. Voir todo.
+		exit 1
 	fi
 }
