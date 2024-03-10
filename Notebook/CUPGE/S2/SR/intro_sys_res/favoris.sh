ff28cd0c5244ed9b5a6224071e2c2330bde87e5f
lethargii <l.sevault@hotmail.com>
2024-03-10 14:11:02 +0100

    03/10/24

--- Notebook/CUPGE/S2/SR/intro_sys_res/favoris.sh
+++ Notebook/CUPGE/S2/SR/intro_sys_res/favoris.sh
@@ -6,12 +6,24 @@ if [ ! -e $FAV ]; then
 fi
 # Sauvegarder un favoris
 function S(){
+	if [ -z $1 ] ; then
+		echo La commande \'S\' s\'exécute avec \'S nom_raccourci\'
+		return 0
+	fi
+	if [ $(cat $FAV | grep ^"$(echo ${@:1})") ] ; then
+		echo Le raccourci \'$1\' existe déja :
+		echo $(cat $FAV | grep ^"$(echo ${@:1})")
+	fi
 	echo Le répertoire $(pwd) est sauvegardé dans vos favoris.
-	echo "	-> raccourci : ${@:1}"
+	echo "-> raccourci : ${@:1}"
 	echo "${@:1}− >$(pwd)" >> $FAV
 }
 # Se déplacer dans un répertoire enregistré dans les favoris
 function C(){
+	if [ -z $1 ] ; then
+		echo La commande \'C\' s\'exécute avec \'C nom_raccourci\'
+		return 0
+	fi
 	CHEMIN=""
 	if [ $(cat $FAV | grep ^"$(echo ${@:1})" | wc -l) -eq 1 ] && [ "$(cat $FAV | grep ^"$(echo ${@:1})")" ]; then
 		CHEMIN="$(cat $FAV | grep ^"$(echo ${@:1})" |  awk -F'− >' '{print $2}')"
@@ -20,7 +32,7 @@ function C(){
 		CHEMIN="$(cat $FAV | grep ^"$(echo ${@:1})"'− >' | awk -F'− >' '{print $2}')"
 	fi
 	if [ -z "$CHEMIN" ] ; then
-		echo Le favoris \"${@:1}\" n\'existe pas.
+		echo Le raccourci \'${@:1}\' n\'existe pas.
 		return 1
 	fi
 	echo Vous êtes maintenant dans le répertoire
@@ -29,9 +41,18 @@ function C(){
 }
 # Supprimer un favoris de la liste
 function R(){
-	cat $FAV | grep -v ^"$(echo ${@:1})" > $TMP
-	cat $TMP > $FAV
-	echo Le favoris \"${@:1}\" est supprimé de votre liste.
+	if [ -z $1 ] ; then
+		echo La commande \'R\' s\'exécute avec \'R nom_raccourci\'
+		return 0
+	else
+		if [ $(grep -c ^$1 $FAV) -eq 0 ] ; then
+			echo Le raccourci \'${@:0}\' n\'existe pas.
+			return 0
+		fi
+		echo Le favori \"${@:1}\" a été supprimé de votre liste.
+		cat $FAV | grep -v ^"$(echo ${@:1})" > $TMP
+		cat $TMP > $FAV
+	fi
 }
 # Lister tous les favoris
 function L(){
