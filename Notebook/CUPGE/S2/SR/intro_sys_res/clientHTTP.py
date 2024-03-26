f2262043be7e7f76ae3ed12395f7bdf5d87b8e56
lethargii <l.sevault@hotmail.com>
2024-03-26 18:32:19 +0100

    03/26/24

---
+++ Notebook/CUPGE/S2/SR/intro_sys_res/clientHTTP.py
@@ -0,0 +1,7 @@
+import socket
+
+
+clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.connect(('localhost', 8080))
+clientsocket.send("GET /index.html HTTP/1.1\r\nHost: localhost\r\n\r\n".encode("utf-8"))
+print(clientsocket.recv(65565).decode("utf-8"))
