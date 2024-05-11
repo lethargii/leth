0fe534b54b1e1724462548a3f77567b2c2a726f0
lethargii <l.sevault@hotmail.com>
2024-05-11 15:52:48 +0200

    05/11/24

--- Notebook/CUPGE/S2/Physique/graphes.py
+++ Notebook/CUPGE/S2/Physique/graphes.py
@@ -8,19 +8,34 @@ L = 0.412
 n = N/L
 I = 3
 
-values = [1.82, 1.79, 1.79, 1.80, 1.80, 1.80, 1.81, 1.80, 1.81, 1.80, 1.81, 1.80, 1.80, 1.78, 1.78, 1.76, 1.74, 1.71, 1.64, 1.52, 1.30, 0.99, 0.64]
+#values = [1.82, 1.79, 1.79, 1.80, 1.80, 1.80, 1.81, 1.80, 1.81, 1.80, 1.81, 1.80, 1.80, 1.78, 1.78, 1.76, 1.74, 1.71, 1.64, 1.52, 1.30, 0.99, 0.64]
 
-z = np.linspace(-0.22, 0.22, 45)
-B_th = mu0*n*I/2*(10**3)*((((L/2)-z)/np.sqrt(((L/2)-z)**2+R**2))+(((L/2)+z)/np.sqrt(((L/2)+z)**2+R**2)))
-B_exp = values[:0:-1] + values
+#z = np.linspace(-0.22, 0.22, 45)
+#B_th = mu0*n*I/2*(10**3)*((((L/2)-z)/np.sqrt(((L/2)-z)**2+R**2))+(((L/2)+z)/np.sqrt(((L/2)+z)**2+R**2)))
+#B_exp = values[:0:-1] + values
 
 fig, ax = plt.subplots()
-ax.plot(z,B_th, label="Courbe théorique")
-ax.plot(z,B_exp, label="Courbe expérimentale")
-ax.set_title("Norme du champ magnétique B(z) en fonction de z")
+#ax.plot(z,B_th, label="Courbe théorique")
+#ax.plot(z,B_exp, label="Courbe expérimentale")
+Ua = [1, 1.5, 2, 3, 3.5, 2.25, 2.75, 3.25]
+Ua.sort()
+Ud = [1.2, 1.85, 2.4, 3.5, 4.15, 2.75, 3.33, 3.80]
+Ud.sort()
+coeff = np.polyfit(Ua, Ud, 1)
+print(coeff)
+Umodel = coeff[0]*np.linspace(-1,5,8)
+ax.plot(Ua,Ud, 'r+')
+ax.plot(np.linspace(-1,5,8), Umodel)
+ax.grid()
+ax.set_title("Ud en fonction de Ua")
 ax.legend()
-ax.set_xlabel("z en m")
-ax.set_ylabel("B(z) en mT")
-ax.xaxis.set_ticks(np.linspace(-0.22, 0.22, 12))
-ax.yaxis.set_ticks(np.linspace(0.4, 1.8, 15))
+#ax.set_xlabel("z en m")
+#ax.set_ylabel("B(z) en mT")
+ax.set_xlabel("Ua en V")
+ax.set_ylabel("Ud en V")
+ax.xaxis.set_ticks(np.linspace(0, 5, 6))
+ax.yaxis.set_ticks(np.linspace(0, 5, 6))
+#ax.yaxis.set_ticks(np.linspace(0.4, 1.8, 15))
+ax.set_xlim(0,5)
+ax.set_ylim(0,5)
 plt.show()
--- Notebook/Linux/mint.sh
+++ Notebook/Linux/mint.sh
@@ -21,3 +21,5 @@ bash flatpak.sh
 git config --global user.email "l.sevault@hotmail.com"
 git config --global user.name "lethargii"
 cp config.fish /home/lethargii/.config/fish/config.fish
+wget https://invent.kde.org/plasma/plasma-workspace-wallpapers/-/raw/master/Altai/contents/images/5120x2880.png
+sudo mv 5120x2880.png /usr/share/backgrounds/linuxmint/Altai.png
