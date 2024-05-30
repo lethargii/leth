a27de2cdf7290f5691096737064eaec4f067f380
lethargii <l.sevault@hotmail.com>
2024-05-30 10:44:40 +0200

    05/30/24

--- Notebook/Linux/mint.sh
+++ Notebook/Linux/mint.sh
@@ -22,7 +22,6 @@ sudo apt upgrade -qq
 bash flatpak.sh
 git config --global user.email "l.sevault@hotmail.com"
 git config --global user.name "lethargii"
-cp config.fish /home/lethargii/.config/fish/config.fish
-pip install --user numpy matplotlib
+cp config.fish $HOME/.config/fish/config.fish
 wget https://invent.kde.org/plasma/plasma-workspace-wallpapers/-/raw/master/Altai/contents/images/5120x2880.png
 sudo mv 5120x2880.png /usr/share/backgrounds/linuxmint/Altai.png
